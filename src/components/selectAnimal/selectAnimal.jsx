3cb51babc74e7c3344586168398e0f31f1caa3fb
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-10-27 18:54:24 +0200

    Create Selects components

---
+++ src/components/selectAnimal/selectAnimal.jsx
@@ -0,0 +1,36 @@
+export function SelectAnimal({
+  textAttribute,
+  value,
+  handleSelect,
+  extraHandleSelect,
+  arrayForOptions,
+}) {
+  return (
+    <div className="control-wrapper">
+      <label className="control-label" htmlFor={`${textAttribute}`}>
+        {`${textAttribute}`}
+      </label>
+      <select
+        className="control-select"
+        id={`${textAttribute}`}
+        value={value}
+        onChange={function (event) {
+          handleSelect(event.target.value);
+          extraHandleSelect("");
+        }}
+        onBlur={function (event) {
+          handleSelect(event.target.value);
+          extraHandleSelect("");
+        }}
+      >
+        <option value="">Select an animal</option>
+        {arrayForOptions &&
+          arrayForOptions.map((element) => (
+            <option key={element.name} value={element.name}>
+              {element.name}
+            </option>
+          ))}
+      </select>
+    </div>
+  );
+}
