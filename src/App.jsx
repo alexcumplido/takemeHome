9db1c7ef620bcde315563f9e10a3b77ad96b6346
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-10-27 18:35:30 +0200

    Pre migration snapshot

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
 import { createRoot } from "react-dom/client";
-import { Pet } from "./Pet.jsx";
-
-// delete the Pet component
-
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Details from "./Details";
+import SearchParams from "./SearchParams";
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </QueryClientProvider>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
