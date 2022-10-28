772791de85fc89b1cd36a1c037146b830ca87e9d
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-10-28 12:27:02 +0200

    Located SelectAnimal in original place

--- src/routes/search/SearchParams.jsx
+++ src/routes/search/SearchParams.jsx
@@ -4,7 +4,7 @@ import { Results } from "../../components/results/Results.jsx";
 import { useBreedList } from "../../utils/useBreedList.js";
 import { Loader } from "../../components/loader/Loader.jsx";
 import { Select } from "../../components/select/Select.jsx";
-// import { SelectAnimal } from "../../components/selectAnimal/SelectAnimal.jsx";
+import { SelectAnimal } from "../../components/selectAnimal/selectAnimal.jsx";
 import { Button } from "../../components/button/Button.jsx";
 
 export function SearchParams() {
@@ -171,42 +171,5 @@ export function SearchParams() {
         />
       </div>
     </section>
-  );
-}
-
-function SelectAnimal({
-  textAttribute,
-  value,
-  handleSelect,
-  extraHandleSelect,
-  arrayForOptions,
-}) {
-  return (
-    <div className="control-wrapper">
-      <label className="control-label" htmlFor={`${textAttribute}`}>
-        {`${textAttribute}`}
-      </label>
-      <select
-        className="control-select"
-        id={`${textAttribute}`}
-        value={value}
-        onChange={function (event) {
-          handleSelect(event.target.value);
-          extraHandleSelect("");
-        }}
-        onBlur={function (event) {
-          handleSelect(event.target.value);
-          extraHandleSelect("");
-        }}
-      >
-        <option value="">Select an animal</option>
-        {arrayForOptions &&
-          arrayForOptions.map((element) => (
-            <option key={element.name} value={element.name}>
-              {element.name}
-            </option>
-          ))}
-      </select>
-    </div>
   );
 }
