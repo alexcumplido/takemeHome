42934135c7a6f69cd8265d4dbc64e2ed19f359d4
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-10-29 17:41:07 +0200

    Remove react query code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { StrictMode, useState } from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Header } from "./components/header/Header.jsx";
 import { Footer } from "./components/footer/Footer.jsx";
 import { Home } from "./routes/home/Home.jsx";
@@ -10,29 +9,19 @@ import { WrappedDetails } from "./routes//details/Details.jsx";
 import { SavedDashboard } from "./routes/saved/SavedDashboard.jsx";
 import { Contact } from "./routes/contact/Contact.jsx";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: Infinity,
-      cacheTime: Infinity,
-    },
-  },
-});
 const App = () => {
   return (
     <StrictMode>
       <BrowserRouter>
-        <QueryClientProvider client={queryClient}>
-          <Header />
-          <Routes>
-            <Route path="/details/:id/:save" element={<WrappedDetails />} />
-            <Route path="/dashboard" element={<SavedDashboard />} />
-            <Route path="/search" element={<SearchParams />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/" element={<Home />} />
-          </Routes>
-          <Footer />
-        </QueryClientProvider>
+        <Header />
+        <Routes>
+          <Route path="/details/:id/:save" element={<WrappedDetails />} />
+          <Route path="/dashboard" element={<SavedDashboard />} />
+          <Route path="/search" element={<SearchParams />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
       </BrowserRouter>
     </StrictMode>
   );
