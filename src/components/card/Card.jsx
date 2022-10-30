8547cb122a45b959790834c83aad53bd03857958
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-10-30 13:00:54 +0100

    Restored card file from commit

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,51 +2,48 @@ import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { ButtonSave } from "../buttonSave/ButtonSave.jsx";
 
-export function Card({ pet }) {
+export function Card(params) {
+  const { pet } = params;
   const [save, setSave] = useState(false);
   let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
   if (pet.photos.length) {
     hero = pet.photos[0].full;
   }
-  function handleSave() {
-    let savedPets = [];
-    if (localStorage.getItem("savedPets")) {
-      savedPets = JSON.parse(localStorage.getItem("savedPets"));
-      const isRepeated = savedPets.find(function (element) {
-        return element.id === pet.id;
+  function handleClick() {
+    let savePets = [];
+    if (localStorage.getItem("savePets")) {
+      savePets = JSON.parse(localStorage.getItem("savePets"));
+    }
+    let repeated = savePets.find(function (element) {
+      return element.id === pet.id;
+    });
+    if (repeated) {
+      savePets = savePets.filter(function (element) {
+        return element.id !== pet.id;
       });
-      if (isRepeated) {
-        savedPets = savedPets.filter(function (element) {
-          return element.id !== pet.id;
-        });
-        setSave(false);
-      } else {
-        savedPets.push(pet);
-        setSave(true);
-      }
+      setSave(false);
     } else {
-      savedPets.push(pet);
+      savePets.push(pet);
       setSave(true);
     }
-    window.localStorage.setItem("savedPets", JSON.stringify(savedPets));
+    window.localStorage.setItem("savePets", JSON.stringify(savePets));
   }
 
   useEffect(
     function () {
-      if (localStorage.getItem("savedPets")) {
-        const savedPets = JSON.parse(localStorage.getItem("savedPets"));
-        const isRepeated = savedPets.find(function (element) {
+      if (localStorage.getItem("savePets")) {
+        let savePets = JSON.parse(localStorage.getItem("savePets"));
+        let repeated = savePets.find(function (element) {
           return element.id === pet.id;
         });
-        isRepeated ? setSave(true) : setSave(false);
+        repeated ? setSave(true) : setSave(false);
       }
     },
     [] //eslint-disable-line react-hooks/exhaustive-deps
   );
-
   return (
     <article className="card">
-      <ButtonSave isPressed={save} handleClick={handleSave} />
+      <ButtonSave save={save} handleClick={handleClick} />
       <Link to={`/details/${pet.id}/${save}`} className="link card__link">
         <img
           className="card__image"
