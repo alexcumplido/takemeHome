ba01a91778f2e993ed3c3177f9816e6680d5161b
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-11-18 17:53:58 +0100

    Create object function wich support api mapping

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,3 @@
-import { Client } from "@petfinder/petfinder-js";
-
 export const inputs = {
   size: ["Small", "Medium", "Large", "Extra Large"],
   age: ["Baby", "Young", "Adult", "Senior"],
@@ -7,16 +5,6 @@ export const inputs = {
   coat: ["Hairless", "Short", "Medium", "Long", "Wire", "Curly"],
   care: ["House-trained"],
 };
-
-const petApi = {
-  API_KEY: "wb3Pi0bmejW1JKL079AroKYF9MCWKzvrHliSTvt7UBbCC1Blch",
-  API_SRC: "SCsspTeddgTKdk3zeP6t8w2Z8rTe6YwyykuAkr1K",
-};
-
-export const client = new Client({
-  apiKey: petApi.API_KEY,
-  secret: petApi.API_SRC,
-});
 
 export function existKeyStorage(key) {
   let exist = false;
@@ -30,77 +18,9 @@ export function retrieveKeyStorage(key) {
   return JSON.parse(localStorage.getItem(key));
 }
 
-export async function requestHomeDogs() {
-  const response = await client.animal
-    .search({
-      type: "Dog",
-      breed: "",
-      size: "",
-      age: "",
-      gender: "",
-      coat: "",
-      house_trained: false,
-      page: 1,
-      limit: 10,
-    })
-    .then(function onFulfillment(responseObject) {
-      return responseObject.data.animals;
-    })
-    .catch(function onRejection(responseObject) {
-      console.log(responseObject);
-    });
-  return response;
-}
-
-export async function fetchTypes() {
-  const result = await client.animalData
-    .types()
-    .then(function onFulfillment(responseObject) {
-      return responseObject.data.types;
-    })
-    .catch(function onRejection(responseObject) {
-      console.log(responseObject);
-    });
-  return result;
-}
-
-export async function fetchAnimals(
-  animal,
-  breed,
-  size,
-  age,
-  gender,
-  coat,
-  care,
-  counterPage = 1
-) {
-  console.log(animal, breed, size, age, gender, coat, care, counterPage);
-  const result = await client.animal
-    .search({
-      type: animal,
-      breed: breed,
-      size: size,
-      age: age,
-      gender: gender,
-      coat: coat,
-      house_trained: care,
-      page: counterPage,
-      limit: 25,
-    })
-    .then(function onFulfillment(responseObject) {
-      console.log(responseObject);
-
-      return responseObject.data;
-    })
-    .catch(function onRejection(responseObject) {
-      console.log(responseObject);
-    });
-  return result;
-}
-
-function cleanResponsePet(responseObject) {
-  let element = responseObject.data.animal;
+export function cleanObject(element) {
   let noData = "No data";
+  console.log(element);
   return {
     id: element.id ?? noData,
     description: element.description ?? noData,
@@ -121,19 +41,8 @@ function cleanResponsePet(responseObject) {
     size: element.size ?? noData,
     tags: element.tags ?? noData,
     status: element.status ?? noData,
+    lastDate: element.status_changed_at ?? noData,
   };
-}
-
-export async function requestPet(id) {
-  const response = await client.animal
-    .show(id)
-    .then(function onFulfillment(responseObject) {
-      return cleanResponsePet(responseObject);
-    })
-    .catch(function onRejection(responseObject) {
-      console.log(responseObject);
-    });
-  return response;
 }
 
 export function _map(list, callback) {
