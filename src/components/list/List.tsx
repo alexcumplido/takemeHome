b957f9f9e2d93acce87d624887793f8607f38784
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-11-20 15:31:03 +0100

    Migrated List to TS

---
+++ src/components/list/List.tsx
@@ -0,0 +1,17 @@
+type TypeList = {
+  items: string [] | [];
+}
+
+export function List(props : TypeList) : JSX.Element {
+  return (
+    <ul className="flex-center">
+      {props.items.length ? (
+        props.items.map(function (element: string) {
+          return <li key={element}>{element} | </li>;
+        })
+      ) : (
+        <span>No data</span>
+      )}
+    </ul>
+  );
+}
