9cab7e0d1bf8af4940854f85872dbac1ff875f80
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-12-02 16:59:28 +0100

    Migrate selectAnimal to TS

---
+++ src/components/selectAnimal/SelectAnimal.tsx
@@ -0,0 +1,38 @@
+type TypeSelectAnimal = {
+  text: string
+  value: string
+  onChange: React.ChangeEventHandler<HTMLSelectElement>
+  extraOnChange: React.ChangeEventHandler<HTMLSelectElement>
+  options: string []
+}
+
+export function SelectAnimal(props: TypeSelectAnimal): JSX.Element {
+  return (
+    <div className="control-wrapper">
+      <label className="control-label" htmlFor={`${props.text}`}>
+        {`${props.text}`}
+      </label>
+      <select
+        className="control-select"
+        id={`${props.text}`}
+        value={props.value}
+        onChange={function (event) {
+          props.onChange(event.target.value);
+          props.extraOnChange("");
+        }}
+        onBlur={function (event) {
+          props.onChange(event.target.value);
+          props.extraOnChange("");
+        }}
+      >
+        <option value="">Select an animal</option>
+        {props.options.length &&
+          props.options.map((element) => (
+            <option key={element} value={element}>
+              {element}
+            </option>
+          ))}
+      </select>
+    </div>
+  );
+}
