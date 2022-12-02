53581b078fb6c549339cbee58398f4336d35ad2e
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-12-02 16:57:34 +0100

    Migrated hero to ts

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-export function Hero() {
+export function Hero(): JSX.Element {
   return (
     <section className="hero">
       <Link to="/search" className="link hero__link flex-center">
